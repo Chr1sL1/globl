016d154fbe352a1471f6f8c556701897c5cf4b36
Chr1sL1 <xingjia_lee@msn.com>
2018-12-12 17:41:18 +0800

    multi thread test

--- src/uni_test/test_ipc.c
+++ src/uni_test/test_ipc.c
@@ -1,8 +1,11 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 #include "syslib/ipc_channel.h"
 #include "syslib/misc.h"
@@ -20,6 +23,12 @@ struct test_msg
 };
 #pragma pack()
 
+
+static long __send_cnt = 0;
+static long __recv_cnt = 0;
+
+static long __start_time = 0;
+
 static void __fill_msg(char* write_buf, int index)
 {
 	struct test_msg* msg = (struct test_msg*)write_buf;
@@ -36,7 +45,9 @@ static void __on_read_msg(const char* buf, unsigned int size, int prod_service_t
 {
 	struct test_msg* msg = (struct test_msg*)buf;
 
-	printf("[%u]: %s, {%d:%d}\n", msg->index, msg->msg, prod_service_type, prod_service_index);
+	printf("[%u]: %s {%d:%d}\n", msg->index, msg->msg, prod_service_type, prod_service_index);
+
+	++__recv_cnt;
 }
 
 static int __send_msg(struct ipc_local_port* prod_port, int count, int prod_service_type, int prod_service_index)
@@ -52,6 +63,8 @@ static int __send_msg(struct ipc_local_port* prod_port, int count, int prod_serv
 
 		rslt = ipc_write_mp(prod_port, write_buf);
 		err_exit(rslt < 0, "write msg failed: %d.", i);
+
+		__sync_add_and_fetch(&__send_cnt, 1);
 	}
 
 	return 0;
@@ -144,14 +157,20 @@ static void* __prod_thread(void* param)
 
 	while(1)
 	{
-		int msg_cnt = random() % 16 + 1;
+		int msg_cnt = random() % 4 + 1;
+
+		struct timeval tv;
+		gettimeofday(&tv, 0);
+
+		if(tv.tv_sec - __start_time > 10)
+			break;
 
 		rslt = __send_msg(prod_port, msg_cnt, 10, index);
 		if(rslt < 0)
 			printf("send msg failed.\n");
 
 		usleep(1000);
-//		err_exit(rslt < 0, "send msg failed: %d.", index);
+		//		err_exit(rslt < 0, "send msg failed: %d.", index);
 	}
 
 
@@ -171,9 +190,14 @@ int test_ipc_channel_multi_prod(int prod_cnt)
 	{
 		.cons_service_type = 1,
 		.cons_service_index = 1,
-		.message_queue_len = 128,
-		.message_count[0 ... MSG_POOL_COUNT - 1] = 1024,
+		.message_queue_len = 32,
+		.message_count[0 ... MSG_POOL_COUNT - 1] = 128,
 	};
+
+	struct timeval tv;
+	gettimeofday(&tv, 0);
+
+	__start_time = tv.tv_sec;
 
 	rslt = ipc_channel_load(cfg.cons_service_type, cfg.cons_service_index);
 	if(rslt < 0)
@@ -189,17 +213,32 @@ int test_ipc_channel_multi_prod(int prod_cnt)
 
 	for(int i = 0; i < prod_cnt; ++i)
 	{
+		cpu_set_t cs;
+
 		rslt = pthread_create(&trds[i], 0, __prod_thread, (void*)i);
 		err_exit(rslt < 0, "create thread failed: %d.", i);
+
+		CPU_SET(i, &cs);
+
+		rslt = pthread_setaffinity_np(trds[i], sizeof(cs), &cs);
+		err_exit(rslt < 0, "set thread cpu failed: %d.", i);
 	}
 
 	__cons_running = 1;
 
 	while(1)
 	{
-		rslt = __read_msg(256);
+		struct timeval tv;
+		gettimeofday(&tv, 0);
+
+		if(tv.tv_sec - __start_time > 13)
+			break;
+
+		rslt = __read_msg(32);
 		usleep(1000);
 	}
+
+	printf("sent: %d, recv: %d\n", __send_cnt, __recv_cnt);
 
 error_ret:
 	return -1;
